ecc82bc6adb9ddb7ccc938937cd7f090da064840
ichi <ichi@verybuy.tw>
2019-08-20 17:38:34 +0800

    Add missing settings for babel

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,9 @@ module.exports = {
     [ '@babel/preset-env', { targets: { node: 'current' } } ],
     '@babel/preset-typescript',
   ],
+  plugins: [
+    "@babel/plugin-proposal-class-properties",
+  ],
   env: {
     production: {
       presets: ['minify'],
